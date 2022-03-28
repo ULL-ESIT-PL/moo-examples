3d1049b6925869229be76e590197f78d0f4301ca
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-03-28 12:38:59 +0100

    minus.js

---
+++ minus.js
@@ -0,0 +1,14 @@
+const { makeLexer } = require("moo-ignore");
+const TokenDef = {
+    WS: {match: /\s+/, lineBreaks: true},
+    comment: /\/\/.*/,
+    number: /[-+]?\d+(?:\.\d*)?(?:[eE][-+]?\d+)?/,
+    minus: '-', 
+    lparen: '(',
+    rparen: ')',
+};
+
+lexer = makeLexer(TokenDef, ["WS", "comment"]);
+lexer.reset('3-2');
+let tokens = [...lexer];
+console.log(tokens.length);
